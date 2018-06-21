a71d4ae4ddf15f47fa07c6cfac04673465bc3177
odax <nr10@albion.edu>
2018-06-21 15:15:50 -0400

    created Header component

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+
+export default class Header extends Component {
+  render() {
+    return (
+        <header className="App-header">
+          <h1 className="App-title">Summy Mummy</h1>
+        </header>
+    )
+  }
+}
+
+
+// import Button from "@material-ui/core/Button";
+
+// function App() {
+//   return (
+//     <Button variant="raised" color="primary">
+//       Hello World
+//     </Button>
+//   );
+// }
+
+// ReactDOM.render(<App />, document.querySelector("#app"));
